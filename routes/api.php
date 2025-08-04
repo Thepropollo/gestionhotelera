844fe96353e25665f8d05cc5985632f4f9aea2bb
thepropollo <ramonb634@gmail.com>
2025-08-03 22:25:47 -0500

    roles completados

--- routes/api.php
+++ routes/api.php
@@ -9,36 +9,33 @@ Route::get('/user', function (Request $request) {
 
 use App\Http\Controllers\Api\ClienteController;
 
-Route::apiResource('clientes', ClienteController::class);
+// Route::apiResource('clientes', ClienteController::class);
 
 use App\Http\Controllers\Api\UsuarioController;
 
-Route::apiResource('usuarios', UsuarioController::class);
+// Route::apiResource('usuarios', UsuarioController::class);
 
 use App\Http\Controllers\Api\RolController;
 
-Route::apiResource('roles', RolController::class);
+// Route::apiResource('roles', RolController::class);
 
 use App\Http\Controllers\Api\HabitacionController;
 
-Route::apiResource('habitaciones', HabitacionController::class);
+// Route::apiResource('habitaciones', HabitacionController::class);
 
 use App\Http\Controllers\Api\MesaController;
 
-Route::apiResource('mesas', MesaController::class);
+// Route::apiResource('mesas', MesaController::class);
 
 use App\Http\Controllers\Api\SalonController;
 
-Route::apiResource('salones', SalonController::class);
+// Route::apiResource('salones', SalonController::class);
 
 use App\Http\Controllers\Api\ServicioController;
 
-Route::apiResource('servicios', ServicioController::class);
+// Route::apiResource('servicios', ServicioController::class);
 
 use App\Http\Controllers\Api\ServicioExtraController;
-
-Route::apiResource('servicios-extra', ServicioExtraController::class);
-
 use App\Http\Controllers\Api\ReservaController;
 
 Route::apiResource('reservas', ReservaController::class);
@@ -57,67 +54,80 @@ use App\Http\Controllers\Api\DetalleFacturaController;
 
 use App\Http\Controllers\Api\ParticipacionController;
 
-Route::apiResource('participaciones', ParticipacionController::class);
-
 use App\Http\Middleware\RolMiddleware;
-
 
 use App\Http\Controllers\Api\AuthController;
 Route::post('/login', [AuthController::class, 'login']);
 
-// // Route::middleware('auth:sanctum')->group(function () {
-// //     Route::post('/logout', [AuthController::class, 'logout']);
+use App\Http\Controllers\Api\DisponibilidadController;
 
-// //     Route::apiResource('clientes', ClienteController::class);
-// //     Route::apiResource('usuarios', UsuarioController::class);
-// //     Route::apiResource('habitaciones', HabitacionController::class);
-// // });
+Route::get('disponibilidad', [DisponibilidadController::class, 'consultar']);
 
-
-// // Solo Administradores
-// Route::middleware([
-//     'auth:sanctum',
-//     RolMiddleware::class . ':Administrador' // Directamente la clase y el parámetro con dos puntos
-// ])->group(function () {
-//     Route::apiResource('usuarios', UsuarioController::class);
-//     Route::apiResource('roles', RolController::class);
-// });
-
-// Admin y Recepcionistas
+// Admin
 Route::middleware([
     'auth:sanctum',
-    RolMiddleware::class . ':Administrador,Usuario' // Directamente la clase y los parámetros
+    RolMiddleware::class . ':Administrador' // Directamente la clase y los parámetros
+])->group(function () {
+    Route::apiResource('clientes', ClienteController::class);
+    Route::apiResource('usuarios', UsuarioController::class);
+    Route::apiResource('habitaciones', HabitacionController::class);
+    Route::apiResource('mesas', MesaController::class);
+    Route::apiResource('salones', SalonController::class);
+    Route::apiResource('reservas', ReservaController::class);
+    Route::get('clientes/{id}/reservas', [ReservaController::class, 'reservasPorCliente']);
+    Route::apiResource('participaciones', ParticipacionController::class);
+    Route::post('reservas/{id}/participaciones', [ReservaController::class, 'asociarParticipantes']);
+    Route::post('reservas/{id}/cancelar', [ReservaController::class, 'cancelar'])->middleware('auth:sanctum');
+    Route::get('reservas/{reservaId}/servicios', [DetalleReservaController::class, 'index']);
+    Route::post('reservas/{reservaId}/servicios', [DetalleReservaController::class, 'store']);
+    Route::put('reservas/{id}/cancelar', [ReservaController::class, 'cancelar']);
+    Route::middleware('auth:sanctum')->post('/facturar', [FacturaController::class, 'facturar']);
+    Route::middleware(['auth:sanctum'])->patch('facturas/{id}/pagar', [FacturaController::class, 'marcarComoPagada']);
+    Route::middleware('auth:sanctum')->get('clientes/{id}/facturas', [FacturaController::class, 'facturasPorCliente']);
+    Route::middleware('auth:sanctum')->post('facturas/consolidar', [FacturaController::class, 'consolidarFactura']);
+    Route::apiResource('servicios-extra', ServicioExtraController::class);
+    Route::put('reservas/{id}/cancelar', [ReservaController::class, 'cancelar']);
+    Route::post('/facturas/completa', [FacturaController::class, 'facturar']);
+    Route::middleware('auth:sanctum')->post('/facturar', [FacturaController::class, 'facturar']);
+});
+
+// Recepcionostas
+Route::middleware([
+    'auth:sanctum',
+    RolMiddleware::class . ':Usuario' // Directamente la clase y los parámetros
 ])->group(function () {
     Route::apiResource('clientes', ClienteController::class);
     Route::apiResource('reservas', ReservaController::class);
     Route::get('clientes/{id}/reservas', [ReservaController::class, 'reservasPorCliente']);
     Route::post('reservas/{id}/participaciones', [ReservaController::class, 'asociarParticipantes']);
     Route::post('reservas/{id}/cancelar', [ReservaController::class, 'cancelar'])->middleware('auth:sanctum');
+    Route::apiResource('participaciones', ParticipacionController::class);
     Route::get('reservas/{reservaId}/servicios', [DetalleReservaController::class, 'index']);
     Route::post('reservas/{reservaId}/servicios', [DetalleReservaController::class, 'store']);
+    Route::put('reservas/{id}/cancelar', [ReservaController::class, 'cancelar']);
+    Route::middleware('auth:sanctum')->post('/facturar', [FacturaController::class, 'facturar']);
+    Route::middleware(['auth:sanctum'])->patch('facturas/{id}/pagar', [FacturaController::class, 'marcarComoPagada']);
+    Route::middleware('auth:sanctum')->get('clientes/{id}/facturas', [FacturaController::class, 'facturasPorCliente']);
+    Route::middleware('auth:sanctum')->post('facturas/consolidar', [FacturaController::class, 'consolidarFactura']);
+    Route::apiResource('servicios-extra', ServicioExtraController::class);
+    Route::put('reservas/{id}/cancelar', [ReservaController::class, 'cancelar']);
+    Route::post('/facturas/completa', [FacturaController::class, 'facturar']);
+    Route::middleware('auth:sanctum')->post('/facturar', [FacturaController::class, 'facturar']);
 });
 
-// 3|eZLq4MawYNyPTT14Tj7tJJRmRcG41x9wUehtibNt65d2c503
 
-// 4|y06UgPemR6T6KgrpNFHQp21PDa2yQCsWW3vpyGRo2c8bd55f
+Route::middleware([
+    'auth:sanctum',
+    RolMiddleware::class . ':Propietario' // Directamente la clase y los parámetros
+])->group(function () {
+    Route::apiResource('clientes', ClienteController::class);
+    Route::apiResource('usuarios', UsuarioController::class);
+    Route::apiResource('roles', RolController::class);
+    Route::apiResource('habitaciones', HabitacionController::class);
+    Route::apiResource('mesas', MesaController::class);
+    Route::apiResource('salones', SalonController::class);
+    Route::apiResource('servicios', ServicioController::class);
+    Route::apiResource('reservas', ReservaController::class);
 
-
-Route::middleware('auth:sanctum')->post('/facturar', [FacturaController::class, 'facturar']);
-
-use App\Http\Controllers\Api\DisponibilidadController;
-
-Route::get('disponibilidad', [DisponibilidadController::class, 'consultar']);
-
-Route::put('reservas/{id}/cancelar', [ReservaController::class, 'cancelar']);
-
-
-
-Route::middleware(['auth:sanctum'])->patch('facturas/{id}/pagar', [FacturaController::class, 'marcarComoPagada']);
-
-Route::middleware('auth:sanctum')->get('clientes/{id}/facturas', [FacturaController::class, 'facturasPorCliente']);
-
-Route::middleware('auth:sanctum')->post('facturas/consolidar', [FacturaController::class, 'consolidarFactura']);
-//1|fKRCu1WceC52iawScl5i3Ct9HWNreDSx0rU07R745c92873d
-
-
-Route::post('/facturas/completa', [FacturaController::class, 'facturar']);
+    Route::get('clientes/{id}/reservas', [ReservaController::class, 'reservasPorCliente']);
+});
